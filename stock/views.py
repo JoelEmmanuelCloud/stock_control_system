35bbc8c2c6f3dc56fa57a349f8371cfcb2dab3ff
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-22 15:51:30 +0100

    feat: create stock views

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Material
 
-# Create your views here.
+def material_list(request):
+    materials = Material.objects.all()
+    return render(request, 'stock/material_list.html', {'materials': materials})
+
