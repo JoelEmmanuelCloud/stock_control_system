c98ce75199c1ebc85af8ac2130bbccb1e7193c74
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-22 16:48:13 +0100

    format: update stock model

--- stock/models.py
+++ stock/models.py
@@ -1,9 +1,11 @@
+# stock/models.py
 from django.db import models
 
 class Material(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField(default=0)
+    category = models.CharField(max_length=50, null=True, blank=True)
+    unit = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
