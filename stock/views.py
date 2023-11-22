8e10a62be29c36b232606554423ba90c2dcfbaae
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-22 17:44:06 +0100

    feat: add chart views class

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from chartjs.views.lines import BaseLineChartView
 from .models import Material
 
 def material_list(request):
@@ -8,3 +9,18 @@ def material_list(request):
 def home(request):
     return render(request, 'stock/home.html')
 
+class MaterialQuantityChartView(BaseLineChartView):
+    def get_labels(self):
+        return [material.name for material in Material.objects.all()]
+
+    def get_data(self):
+        return [material.quantity for material in Material.objects.all()]
+
+    def get_colors(self):
+        return [{'border_color': 'rgba(75, 192, 192, 1)', 'background_color': 'rgba(75, 192, 192, 0.2)'}]
+
+    def get_datasets(self, **kwargs):
+        data = self.get_data()
+        return [{'data': data, **kwargs}]
+
+material_quantity_chart = MaterialQuantityChartView.as_view()
