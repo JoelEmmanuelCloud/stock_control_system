7f1f23bc730266027180296af07c7330298cbdf4
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-22 15:47:49 +0100

    feat: define stock model

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
