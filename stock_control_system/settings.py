ba2da1c0b8e6bb0d821ab8b6cde206e6051bbe0f
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-23 02:02:23 +0100

    refactor: update settings

--- stock_control_system/settings.py
+++ stock_control_system/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'stock.apps.StockConfig',
     'stock',
     'chartjs',
 ]
@@ -50,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'stock_control_system.urls'
