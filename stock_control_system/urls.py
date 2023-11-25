025e0d9744e4f78cebcad482944b223ea98b4cab
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-25 10:40:36 +0100

    feat: add registration app in project urls

--- stock_control_system/urls.py
+++ stock_control_system/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from register import views as v
+from registration import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', v.register, name="register"),
     path('', include('stock.urls')),
+      path('', include('registration.urls')), 
 ]
