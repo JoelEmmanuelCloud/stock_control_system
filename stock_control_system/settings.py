9306242719f167a2b68afd8971574e27b4fe1e03
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-25 10:40:05 +0100

    feat: add login and logout fields to settings

--- stock_control_system/settings.py
+++ stock_control_system/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'stock',
     'chartjs',
+    'registration',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'home'
